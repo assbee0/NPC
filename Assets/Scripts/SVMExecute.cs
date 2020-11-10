f1153024d6950911e0c1ed86cda2a5e7d14091db
ryo.oji <tendonman519@gmail.com>
2020-11-10 14:37:25 +0900

    SVMExecute.csの作成、gitignoreの編集（Parameterもプッシュされるように）

---
+++ Assets/Scripts/SVMExecute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SVMExecute : MonoBehaviour
+{
+    const int CLASSNUM = 2;
+    const int HUNITNUM = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Readw()
+    {
+        string filepath = "C:\git\NPC 2.0\Assets\SVM\Parameters";
+        if (!File.Exists(filepath))
+            return;
+        StreamReader sr = new StreamReader(filepath, Encoding.UTF8);
+        for(int i = 0; i < HUNITNUM; i++)
+        {
+            w[i] = new float[CLASSNUM];
+            string wline = sr.ReadLine();
+            string[] ws = wline.Split(' ');
+            w[i][0] = float.Parse(ws[0]);
+            w[i][1] = float.Parse(ws[1]);
+            //w[i][2] = float.Parse(ws[2]);
+        }
+        sr.Close();
+    }
+}
