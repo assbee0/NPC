67a30e82ae13752b71065a9e0ff53d47ac3df833
ryo.oji <tendonman519@gmail.com>
2020-11-20 01:49:18 +0900

    Unity上でのSVMの実装、Blendtreeとのパラメタの連携

---
+++ Assets/Scripts/NPCAnimationController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimationController : MonoBehaviour
+{
+    public float arousal;
+    public float valence;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Execute();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Execute();
+    }
+
+    public void Execute()
+    {
+        SVMExecute svme = GetComponent<SVMExecute>();
+        svme.Predict();
+        arousal = getParameter(svme.result_A);
+        valence = getParameter(svme.result_V);
+        animator.SetFloat("Arousal",arousal);
+        animator.SetFloat("Valence",valence);
+        Debug.Log(arousal);
+        Debug.Log(valence);
+    }
+
+    public float getParameter(int x)
+    {
+        float boundary = 256.0f / 3.0f;
+        if (x == 0) {
+            return Random.Range(0.0f, boundary);
+        } else if (x == 1) {
+            return Random.Range(boundary, boundary * 2.0f);
+        } else if (x == 2) {
+            return Random.Range(boundary * 2.0f, 256.0f);
+        } else {
+            return -1;
+        }
+    }
+}
--- Assets/Scripts/SVMExecute.cs
+++ Assets/Scripts/SVMExecute.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
 
 public class SVMExecute : MonoBehaviour
 {
-    const int CLASSNUM = 2;
-    const int HUNITNUM = 150;
+    const int CLASSNUM = 3;
+    public int result_A = -1;
+    public int result_V = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +23,153 @@ public class SVMExecute : MonoBehaviour
         
     }
 
-    public void Readw()
+    public void Predict()
     {
-        string filepath = "C:\git\NPC 2.0\Assets\SVM\Parameters";
+        // Arousalについて
+        //ParameterManage pm = GetComponent<ParameterManage>();
+        //Hunit hunit = new Hunit();
+        OutunitTest outunit_A = new OutunitTest();
+        float[] input_A = new float[2] {1, 7}; // ここをどうにかする！！！！！！！！！！
+        //input = ParaTransform(input);
+        //outunit.Readw();
+        outunit_A.Readb();
+        outunit_A.Readw();
+        outunit_A.Propagation(input_A);
+        //outunit.Propagation(hunit.output);
+        result_A = Argmax(outunit_A.output); // result：カテゴリ
+        //EvaluateOutput();
+
+        // Valenceについて
+        OutunitTest outunit_V = new OutunitTest();
+        float[] input_V = new float[2] {1, 7}; // ここをどうにかする！！！！！！！！！！
+        //input = ParaTransform(input);
+        //outunit.Readw();
+        outunit_V.Readb();
+        outunit_V.Readw();
+        outunit_V.Propagation(input_V);
+        //outunit.Propagation(hunit.output);
+        result_V = Argmax(outunit_V.output); // result：カテゴリ
+        //EvaluateOutput();
+    }    
+
+    public static float Dot(float[] a, float[] b)
+    {
+        if (a.Length != b.Length)
+            return 0;
+        float d = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            d += a[i] * b[i];
+        }
+        return d;
+    }
+
+    public static float StepFunction(float x)
+    {
+        if (x > 0)
+            return 1;
+        else
+            return 0;
+    }
+
+    public static float[] Softmax(float[] x)
+    {
+        float[] y = new float[CLASSNUM];
+        float sum = 0;
+        for(int i=0; i<x.Length;i++)
+        {
+            x[i] = Mathf.Exp(x[i]);
+            sum += x[i];
+        }
+        for (int i = 0; i < x.Length; i++)
+        {
+            y[i] = x[i] / sum;
+        }
+        return y;
+    }
+
+    private int Argmax(float[] input)
+    {
+        float max = -1;
+        int argmax = -1;
+        for(int i = 0; i < input.Length; i++)
+        {
+            if (input[i] > max)
+            {
+                max = input[i];
+                argmax = i;
+            }
+        }
+        return argmax+1;
+    }
+}
+
+class OutunitTest
+{
+    const int CLASSNUM = 3;
+    const int HUNITNUM = 2;
+    public float[] b = new float[CLASSNUM];
+    public float[][] w = new float[CLASSNUM][];
+    public float[] u = new float[CLASSNUM];
+    public float[] output = new float[CLASSNUM];
+
+    void Start()
+    {
+
+    }
+
+    public void Propagation(float[] x)
+    {
+        for (int i = 0; i < CLASSNUM; i++)
+        {
+            u[i] = SVMExecute.Dot(getRow(w, i), x) + b[i];
+            output[i] = SVMExecute.StepFunction(u[i]);
+        }
+    }
+
+    public void Readb()
+    {
+        string filepath = Application.dataPath + "/SVM/Parameters/A_intercept.txt";
         if (!File.Exists(filepath))
             return;
         StreamReader sr = new StreamReader(filepath, Encoding.UTF8);
-        for(int i = 0; i < HUNITNUM; i++)
+        for (int i = 0; i < CLASSNUM; i++)
         {
-            w[i] = new float[CLASSNUM];
             string wline = sr.ReadLine();
-            string[] ws = wline.Split(' ');
-            w[i][0] = float.Parse(ws[0]);
-            w[i][1] = float.Parse(ws[1]);
-            //w[i][2] = float.Parse(ws[2]);
+            b[i] = float.Parse(wline);
         }
         sr.Close();
+    }
+
+    public void Readw() // coefが重みW
+    {
+        string filepath = Application.dataPath + "/SVM/Parameters/A_coef.txt";
+        if (!File.Exists(filepath))
+            return;
+        StreamReader sr = new StreamReader(filepath, Encoding.UTF8);
+        for(int i = 0; i < CLASSNUM; i++)
+        {
+            w[i] = new float[HUNITNUM];
+            string wline = sr.ReadLine();
+            string[] ws = wline.Split(' ');
+            for(int j = 0; j < HUNITNUM; j++)
+            {
+                w[i][j] = float.Parse(ws[j]);
+            }
+        }
+        sr.Close();
+    }
+
+    private float[] getRow(float[][] x, int row)
+    {
+        //Debug.Log(x.Length);
+        float[] a = new float[x[0].Length];
+        for (int i = 0; i < x[0].Length; i++)
+        {
+            Debug.Log(i);
+            a[i] = x[row][i]; // 修正したけど、これ合ってる？
+            Debug.Log(a[i]);
+        }
+        return a;
     }
 }
