e7e71def8d9836ea1da8e194def7371719f317b9
ryo.oji <tendonman519@gmail.com>
2020-11-21 05:59:00 +0900

    GetSVMParameterが1次元配列を返すように変更、変数名の調整

--- Assets/Scripts/GetSVMParameter.cs
+++ Assets/Scripts/GetSVMParameter.cs
@@ -4,46 +4,73 @@ using UnityEngine;
 
 public class GetSVMParameter : MonoBehaviour
 {
-    NPCAnimationController script;
+    NPCAnimationController NPCAnimCon;
     public int radius = 100;
-    private int VARNUM = 3; // 取得するパラメタの数
+    private float[] arrayParam;
+    private int VARNUM = 4; // 取得するパラメタの数
+    public int Param1 = 1; // 仮（Debug用）
+    public int Param2 = 5; // 仮（Debug用）
 
     // Start is called before the first frame update
     void Start()
     {
-        //getAroundNPCValue();
+        arrayParam = new float[VARNUM];
+        //getAroundAVValue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        getAroundNPCValue();
+        float[] tmpArray = getAroundAVValue();
+        arrayParam[0] = tmpArray[0];
+        arrayParam[1] = tmpArray[1];
+        arrayParam[2] = Param1;
+        arrayParam[3] = Param2;
     }
 
-    public float[,] getAroundNPCValue()
+    /*
+    arrayParam[0]: 周囲のArousal値
+    arrayParam[1]: 周囲のValence値
+    arrayParam[2]: Param1(仮)
+    arrayParam[3]: Param2(仮)
+    */
+
+    public float[] getAroundAVValue()
     {
         // 中心:自分の位置, 半径:radiusの球内に存在するものを検出
-        Collider[] targets_cld = Physics.OverlapSphere(transform.position, radius);
-        List<GameObject> targets = new List<GameObject>();
-        // 検出したGameObjectの内、tagが"NPC"であるものをtargetsリストに追加
-        foreach (Collider cld in targets_cld) {
+        Collider[] arrayCld = Physics.OverlapSphere(transform.position, radius);
+        List<GameObject> listObj = new List<GameObject>();
+        // 検出したGameObjectの内、tagが"NPC"であるものをlistObjリストに追加
+        foreach (Collider cld in arrayCld) {
             if (cld.tag == "NPC") {
-                targets.Add(cld.gameObject);
+                listObj.Add(cld.gameObject);
             }
         }
 
-        int targetCount = targets.Count;
-        float[,] parameters = new float[targetCount, VARNUM];
-        int i = 0;
-        
-        foreach (GameObject obj in targets) {
-            // 対象となるGameObject(Collider)との距離を調べる
-            parameters[i, 0] = Vector3.Distance(obj.transform.position, transform.position);
-            // 対象となるGameObject(Collider)のA-V値を調べる
-            script = obj.gameObject.GetComponent<NPCAnimationController>();
-            parameters[i, 1] = script.Arousal;
-            parameters[i, 2] = script.Valence;
+        float[] avValue = new float[2];
+
+        foreach (GameObject obj in listObj) {
+            // 対象となるGameObjectとの距離を調べる
+            float dist = Vector3.Distance(obj.transform.position, transform.position);
+            // 対象となるGameObjectのA-V値を調べる
+            NPCAnimCon = obj.gameObject.GetComponent<NPCAnimationController>();
+            float arousal = NPCAnimCon.Arousal;
+            float valence = NPCAnimCon.Valence;
+
+            // 周囲の累計A-V値を更新
+            updateAVValue(avValue, dist, arousal, valence);
         }
-        return parameters;
+        return avValue;
+    }
+
+    public void updateAVValue(float[] avValue, float dist, float ar, float va)
+    {
+        avValue[0] = avValue[0] + (ar - avValue[0]) / dist;
+        avValue[1] = avValue[1] + (va - avValue[1]) / dist;
+    }
+
+    public float[] ArrayParm {
+        get{ return arrayParam; }
+        set{ arrayParam = value;}
     }
 }
